39abbc8dd084a2d944a2a6b0271c421afe7b4802
Daniel Guzman <dguzman1012@gmail.com>
2024-05-22 17:17:46 -0300

    test: Add failing sum test

--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -7,3 +7,7 @@ test("adds 1 + 2 to equal 3", () => {
 test("subtracts 2 from 3 to equal 1", () => {
   expect(decrement(3, 2)).toBe(1);
 });
+
+test("should fail the sum test", () => {
+  expect(sum(1, 2)).toBe(4);
+});
